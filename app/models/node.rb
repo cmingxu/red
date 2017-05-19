3b4c1a5535bcc122c8cea0a0cc18a85e0025c218
kevin xu <cming.xu@gmail.com>
2017-05-19 17:13:27 +0800

    add upload pic
    Signed-off-by: kevin xu <cming.xu@gmail.com>

--- app/models/node.rb
+++ app/models/node.rb
@@ -9,22 +9,5 @@
 #  updated_at :datetime         not null
 #
 
-$GRAPHNA_PANEL = {
-  docker_container_cpu_load: { id: 4, interval: 5.minutes, query: "count_scalar(container_memory_usage_bytes{{app-name}}&start=1494927915&end=1494928815&s" },
-  docker_container_cpu_core: { id: 7, interval: 5.minutes },
-  docker_container_mem_load: { id: 5, interval: 5.minutes },
-  docker_container_mem_used: { id: 2, interval: 5.minutes },
-  docker_container_disk_load: { id: 6, interval: 5.minutes },
-  docker_container_disk_used: { id: 3, interval: 5.minutes },
-  docker_container_running_containers: { id: 9, interval: 1.minutes },
-  docker_container_system_load: { id: 10, interval: 1.minutes },
-  docker_container_io_usage: { id: 15, interval: 1.minutes },
-  docker_container_container_cpu_usage: { id: 8, interval: 1.minutes },
-  docker_container_container_mem_usage: { id: 11, interval: 1.minutes },
-  docker_container_cached_mem_usaged: { id: 12, interval: 1.minutes },
-  docker_container_network_input: { id: 13, interval: 1.minutes },
-  docker_container_network_output: { id: 14, interval: 1.minutes },
-}
-
 class Node < ApplicationRecord
 end
