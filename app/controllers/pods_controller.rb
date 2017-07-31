c0e274f68745ed3982dce43ea69e2ebb878d74e0
kevin xu <cming.xu@gmail.com>
2017-07-31 14:22:34 +0800

    foobar
    Signed-off-by: kevin xu <cming.xu@gmail.com>

--- app/controllers/pods_controller.rb
+++ app/controllers/pods_controller.rb
@@ -5,4 +5,29 @@ class PodsController < ApplicationController
 
   def new
   end
+
+  def create
+    service = Kubeclient::Pod.new
+    service.metadata = {}
+    service.metadata.name = "nginx-#{rand 1000}"
+    service.metadata.namespace = "default"
+    service.spec = {}
+    service.spec.ports =  [{
+      'port' => 80,
+      'targetPort' => 'web'
+    }]
+    service.spec.containers = [
+      {
+        image: "nginx:latest",
+        name: "foobar"
+      }
+    ]
+    fo = @k8client.create_pod(service)
+    redirect_to pods_path
+  end
+
+  def destroy
+    fo = @k8client.delete_pod(params[:name])
+    redirect_to pods_path
+  end
 end
